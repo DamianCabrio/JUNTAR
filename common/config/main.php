e91a97caeffcbea7f1829d39cc1af40eb6cc6a30
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-01 12:11:50 -0300

    Configuracion authManager
    Configurados los parámetros de authManager para trabajar sobre tablas propias

--- common/config/main.php
+++ common/config/main.php
@@ -1,14 +1,28 @@
 <?php
+
 return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'name' => 'Juntar',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        
+        //configuramos el nombre modificado de las tablas que utilizará authManager
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+//            'class' => 'vendor\yourVenderName\auth\models\DbManager',
+            'cache' => 'cache',
+            
+            //renombramos las tablas auth  
+            'itemTable' => 'permiso',
+            'assignmentTable' => 'usuario_rol',
+            'itemChildTable' => 'permiso_rol',
+            'ruleTable' => 'regla',
         ],
     ],
 ];
