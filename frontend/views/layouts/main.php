45ec9add832c848358d746c4bde433eb2ce193f6
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-04 12:00:03 -0300

    Meta tag: charset
    Completado el codigo de la etiqueta para responder a versiones anteriores a html5

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,7 +16,7 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo Yii::$app->charset;?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
