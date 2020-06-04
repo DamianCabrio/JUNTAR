0973702d62a6d4a28d230f9a490b14ea07e82ee6
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-04 11:57:53 -0300

    Modificacion Reestablecer contraseña
    Modificado el estilo del sector reestablecer contraseña para mantener el mismo patrón en todo el sitio. Personalizados los mensajes de alerta y el email.

--- frontend/views/site/requestPasswordResetToken.php
+++ frontend/views/site/requestPasswordResetToken.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap4\ActiveForm */
 /* @var $model \frontend\models\PasswordResetRequestForm */
@@ -7,23 +6,26 @@
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
 
-$this->title = 'Request password reset';
+$this->title = 'Solicitar cambio de contraseña';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-request-password-reset">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 class="text-center"><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out your email. A link to reset password will be sent there.</p>
+    <p class="text-center">Por favor, ingrese su cuenta de correo.</p>
 
     <div class="row">
-        <div class="col-lg-5">
+        <div class="col-lg-5 m-auto">
             <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
 
-                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
-                </div>
+            <div  class="form-advice">
+                <p class=""> Se le enviará un correo a su cuenta para reestablecer la contraseña </p>
+            </div>
+            <div class="form-group">
+                <?= Html::submitButton('Enviar', ['class' => 'btn btn-primary']) ?>
+            </div>
 
             <?php ActiveForm::end(); ?>
         </div>
