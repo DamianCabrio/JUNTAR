1521e0da9e07f558f994912f396aefcf4ae0db58
n-stange <64751066+n-stange@users.noreply.github.com>
2020-06-06 16:28:37 -0300

    ohoh

--- frontend/views/evento/_form.php
+++ frontend/views/evento/_form.php
@@ -4,11 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-<<<<<<< HEAD
 /* @var $model common\models\Evento */
-=======
-/* @var $model common\models\evento */
->>>>>>> c0503ad64a517a0a11aecb6b2fd47fe90b2ea636
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
