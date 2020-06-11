a6c295b844abf548ccb0c45eae538dbca82570b0
Lean <leandro.casanova@est.fi.uncoma.edu.ar>
2020-06-11 01:11:28 -0300

    Update Autocomplete - Registro

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -5,6 +5,8 @@
 
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
+use yii\jui\AutoComplete;
+use yii\web\JsExpression;
 //use yii\jui\DatePicker;
 
 $this->title = 'Crear cuenta';
@@ -25,6 +27,20 @@ $this->params['breadcrumbs'][] = $this->title;
                     <?= $form->field($model, 'nombre')->label('Nombre (*)')->textInput(['placeholder' => 'Ejemplo: Juan']) ?>
                     <?= $form->field($model, 'apellido')->label('Apellido (*)')->textInput(['placeholder' => 'Ejemplo: Perez']) ?>
                     <?= $form->field($model, 'dni')->label('DNI (*)')->textInput(['placeholder' => 'Ejemplo: 26734824']); ?>
+                    <?= $form->field($model, 'pais')->label('Pais (*)')->textInput(['value' => 'Argentina']); ?>
+                    <?= $form->field($model, 'provincia')->label('Provincia (*)')->widget(AutoComplete::classname(), [
+                      'model' => $model,
+                      'attribute' => 'provincia',
+                      'options' => ['class' => 'form-control'],
+                                  'clientOptions' => [
+                                    'source' => $province,
+                                    'autoFill'=>true,
+                                    'minLength'=>'3',
+                                    'options' => ['class' => 'form-control'],
+                                    'select' => new JsExpression("function( event, ui ) {
+                                    $('#signupform-provincia').val(ui.item.id);
+                                  }")],
+                                  ]);?>
                     <?= $form->field($model, 'localidad')->label('Localidad (*)')->textInput(['placeholder' => 'Ejemplo: Neuquen']); ?>
                     <?= $form->field($model, 'email')->input('email')->label('Email (*)')->textInput(['placeholder' => 'Ejemplo: myEmail@gmail.com']); ?>
                     <?= $form->field($model, 'password')->passwordInput([
--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -3,37 +3,62 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function () {
+$(document).ready(function() {
 
-    $('.showpw .custom-control-input').click(function () {
-        var type = $('#signupform-password').attr("type");
+  $('.showpw .custom-control-input').click(function() {
+    var type = $('#signupform-password').attr("type");
 
-        //verificamos si viene por signup y cambiamos el tipo de campo
-        if (type !== null && type === 'password') {
-            $('#signupform-password').attr("type", "text");
-        } else {
-            $('#signupform-password').attr("type", "password");
-        }
+    //verificamos si viene por signup y cambiamos el tipo de campo
+    if (type !== null && type === 'password') {
+      $('#signupform-password').attr("type", "text");
+    } else {
+      $('#signupform-password').attr("type", "password");
+    }
 
-        //verificamos si viene por login y cambiamos el tipo de campo
-        type = $('#loginform-password').attr("type");
-        if (type !== null && type === 'password') {
-            $('#loginform-password').attr("type", "text");
-        } else {
-            $('#loginform-password').attr("type", "password");
-        }
+    //verificamos si viene por login y cambiamos el tipo de campo
+    type = $('#loginform-password').attr("type");
+    if (type !== null && type === 'password') {
+      $('#loginform-password').attr("type", "text");
+    } else {
+      $('#loginform-password').attr("type", "password");
+    }
 
-        //verificamos si viene por resetPassword y cambiamos el tipo de campo
-        type = $('#resetpasswordform-password').attr("type");
-        if (type !== null && type === 'password') {
-            $('#resetpasswordform-password').attr("type", "text");
-        } else {
-            $('#resetpasswordform-password').attr("type", "password");
-        }
+    //verificamos si viene por resetPassword y cambiamos el tipo de campo
+    type = $('#resetpasswordform-password').attr("type");
+    if (type !== null && type === 'password') {
+      $('#resetpasswordform-password').attr("type", "text");
+    } else {
+      $('#resetpasswordform-password').attr("type", "password");
+    }
+  });
+  $(function() {
+    $("[data-toggle='popover']").popover({
+      trigger: 'hover'
     });
-    $(function () {
-        $("[data-toggle='popover']").popover({
-          trigger : 'hover'
-        });
-    });
+  });
+
+  function eliminarDiacriticos(texto) {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+  }
+
+  $("#signupform-provincia").change(function() {
+    var name_province = eliminarDiacriticos($("#signupform-provincia").val());
+    $.ajax({
+        url: "http://yii.juntar.net:8080/index.php?r=site%2Fsearch-locations&name=" + name_province,
+        type: "POST",
+        dataType: "json"
+      })
+      .done(function(data) {
+        if ($("#signupform-localidad").autocomplete !== undefined) {
+          $("#signupform-localidad").autocomplete({
+            "autoFill": true,
+            "minLength": "3",
+            "source": data,
+            "select": function(event, ui) {
+              $("#signupform-localidad").val(ui.item.id);
+            }
+          });
+        }
+      });
+  });
 });
