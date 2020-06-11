c6abbda8e8166334b659960a7daff8c6fd4d261f
Lean <leandro.casanova@est.fi.uncoma.edu.ar>
2020-06-11 00:58:08 -0300

    Add Validator - common/components

--- common/components/ProvinceValidator.php
+++ common/components/ProvinceValidator.php
@@ -2,23 +2,23 @@
 namespace common\components;
 use yii\validators\Validator;
 
-class LocationValidator extends Validator{
+class ProvinceValidator extends Validator{
 public function init() {
         parent::init ();
-        $this->message = 'La localidad ingresada es invalida.';
+        $this->message = 'La Provincia ingresada es invalida.';
     }
 
     public function validateAttribute( $model , $attribute ) {
-
+  
     }
 
     public function clientValidateAttribute( $model , $attribute , $view )
     {
+      $location = $model->$attribute;
       $message = json_encode($this->message, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
       return <<<JS
-        deferred.push($.getJSON('https://apis.datos.gob.ar/georef/api/localidades', {'nombre':$("#signupform-localidad").val()}, function(json){
-            console.log(json.cantidad);
-            if (json.cantidad == '0') {
+        deferred.push($.getJSON('https://apis.datos.gob.ar/georef/api/provincias', {'nombre':$("#signupform-provincia").val()}, function(json){
+            if (json.cantidad == '0' && json.total == '0') {
               console.log($message);
               messages.push($message);
             }
