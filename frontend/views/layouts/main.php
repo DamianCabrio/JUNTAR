f16c87ea80afee2dcb4656890986413622f0b99a
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-12 00:36:24 -0300

    Update --> Signup form
    Validacion de provincia y localidad utilizan funciones js para obtener la lista de posibles valores a ingresar.

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -19,6 +19,9 @@ AppAsset::register($this);
     <meta http-equiv="Content-Type" content="text/html; charset=<?php echo Yii::$app->charset; ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" rel="Stylesheet"></link>
+    <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js" ></script>
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
@@ -70,54 +73,21 @@ AppAsset::register($this);
         NavBar::end();
         ?>
 
-        <div class="container">
-            <?=
-                Breadcrumbs::widget([
-                    'itemTemplate' => "\n\t<li class=\"breadcrumb-item\"><i>{link}</i></li>\n", // template for all links
-                    'activeItemTemplate' => "\t<li class=\"breadcrumb-item active\">{link}</li>\n", // template for the active link
-                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-                ])
+        <!--<div class="container">-->
+            <?php // echo
+//                Breadcrumbs::widget([
+//                    'itemTemplate' => "\n\t<li class=\"breadcrumb-item\"><i>{link}</i></li>\n", // template for all links
+//                    'activeItemTemplate' => "\t<li class=\"breadcrumb-item active\">{link}</li>\n", // template for the active link
+//                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+//                ])
             ?>
-            <?= Alert::widget() ?>
+            <?php // echo Alert::widget() ?>
 
-        </div>
-        <? echo Yii::getAlias('@web');?>
-        <?= $content ?>
+        <!--</div>-->
+        <?php // echo Yii::getAlias('@web');?>
+        <?php echo $content ?>
     </div>
-    <section class="darkish_bg text-light">
-        <div class="container" style="padding-bottom: 4vh;">
-            <div class="row">
-                <div class="col-12 col-md-5" style="padding-top: 4vh; padding-bottom: 4vh;">
-                    <?= Html::img('images/juntar-logo-b.svg',  ['class' => 'img-fluid']); ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-md-8">
-                    <h5 class="white-text">Juntar</h5>
-
-                    <p class="grey-text text-lighten-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada vitae erat a lobortis. Donec pretium tortor eros, a auctor massa suscipit vel. Donec augue odio, vulputate et egestas fringilla, varius vel ligula. Aliquam eu sagittis nisi, vitae imperdiet lorem. Vivamus lobortis diam vel sapien aliquet, dictum placerat tellus imperdiet.</p>
-
-                </div>
-                <div class="col-12 col-md-4">
-                    <h5 class="white-text">Contacto</h5>
-                    <ul>
-                        <li>
-                            <a class="white-text" href="#!">juntar@fi.uncoma.edu.ar</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <hr>
-            <div class="row" style="padding-top: 4vh;padding-bottom: 2vh;">
-                <div class="col s12 m6">
-                    <?= Html::img('images/uncoma.png',  ['class' => 'img-fluid']); ?>
-                </div>
-                <div class="col s12 m6">
-                    <?= Html::img('images/fai.png',  ['class' => 'img-fluid']); ?>
-                </div>
-            </div>
-        </div>
-    </section>
+    
     <footer class="footer dark_bg text-light">
 
         <div class="container">
