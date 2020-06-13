e0987771daa98ffe873ae9934d4ba714f1fc6e34
n-stange <64751066+n-stange@users.noreply.github.com>
2020-06-13 17:55:51 -0300

    filtro por nombre de organizador
    al filtro de eventos en el home se le agrega por nombre de organizador

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -37,12 +37,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-       'urlManager' => [
-           'enablePrettyUrl' => true,
-          'showScriptName' => false,
-           'rules' => [
-           ],
-       ],
+        //  'urlManager' => [
+        //     'enablePrettyUrl' => true,
+        //    'showScriptName' => false,
+        //     'rules' => [
+        //      ],
+        //  ],
     ],
     'params' => $params,
 ];
