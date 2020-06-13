dd759818b7ef813d8cf2112a2c22f66987ef5f49
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-06-13 20:29:59 -0300

    botones ver evento, cargar nueva presentacion, editar evento y publicar evento

--- frontend/views/presentacion/mostrarPresentacion.php
+++ frontend/views/presentacion/mostrarPresentacion.php
@@ -13,15 +13,20 @@ $this->title = $model->tituloPresentacion;
 <div class="container">
     <div class = "row">
         <div class = "col-md-8 col-12 m-auto">
-             <h2 class="text-center">Presentacion cargada</h2>
+             <h2 class="text-center">Presentación cargada</h2>
                 <div class="p-3 mb-2 bg-success text-white text-center">
-                    Su presentacion ha sido cargado exitosamente
+                    Su presentación ha sido cargado exitosamente
                 </div>
                 <br>
                 <hr>
-                <div>
-                 <?= Html::a('Cargar nueva Presentacion', ['cargar-presentacion', 'idEvento' => $evento->idEvento], ['class' => 'btn btn-outline-success btn-sm']) ?>
-                </div>
+                <div class="d-flex justify-content-center">
+                    <div>
+                        <?= Html::a('Nueva Presentación', ['cargar-presentacion', 'idEvento' => $evento->idEvento], ['class' => 'btn btn-outline-success btn-sm']) ?>
+                        <?= Html::a('Ver evento', ['evento/ver-evento', 'idEvento' => $evento->idEvento], ['class' => 'btn btn-outline-success btn-sm']) ?>
+                        <?= Html::a('Editar evento ', ['evento/editar-evento', 'idEvento' => $evento->idEvento], ['class' => 'btn btn-outline-success btn-sm']) ?>
+                        <?= Html::a('Publicar evento ', ['evento/publicar-evento', 'idEvento' => $model->idEvento], ['class' => 'btn btn-outline-success btn-sm']) ?>
+                    </div>
+                </div>    
         </div>
     </div>
 </div>
