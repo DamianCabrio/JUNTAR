83e9fe552e60371bca5b1c4ebafdea01a0d75a7d
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-13 21:15:11 -0300

    Update signupForm
    Actualizado para responder correctamente al ingreso de pais, provincia y localidad

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -3,59 +3,63 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('.showpw .custom-control-input').click(function() {
-    var type = $('#signupform-password').attr("type");
+    $('.showpw .custom-control-input').click(function () {
+        var type = $('#signupform-password').attr("type");
 
-    //verificamos si viene por signup y cambiamos el tipo de campo
-    if (type !== null && type === 'password') {
-      $('#signupform-password').attr("type", "text");
-    } else {
-      $('#signupform-password').attr("type", "password");
-    }
+        //verificamos si viene por signup y cambiamos el tipo de campo
+        if (type !== null && type === 'password') {
+            $('#signupform-password').attr("type", "text");
+        } else {
+            $('#signupform-password').attr("type", "password");
+        }
 
-    //verificamos si viene por login y cambiamos el tipo de campo
-    type = $('#loginform-password').attr("type");
-    if (type !== null && type === 'password') {
-      $('#loginform-password').attr("type", "text");
-    } else {
-      $('#loginform-password').attr("type", "password");
-    }
+        //verificamos si viene por login y cambiamos el tipo de campo
+        type = $('#loginform-password').attr("type");
+        if (type !== null && type === 'password') {
+            $('#loginform-password').attr("type", "text");
+        } else {
+            $('#loginform-password').attr("type", "password");
+        }
 
-    //verificamos si viene por resetPassword y cambiamos el tipo de campo
-    type = $('#resetpasswordform-password').attr("type");
-    if (type !== null && type === 'password') {
-      $('#resetpasswordform-password').attr("type", "text");
-    } else {
-      $('#resetpasswordform-password').attr("type", "password");
-    }
-  });
-  // Habilitacion de los popover
-  $(function() {
-    $("[data-toggle='popover']").popover({
-      trigger: 'hover'
+        //verificamos si viene por resetPassword y cambiamos el tipo de campo
+        type = $('#resetpasswordform-password').attr("type");
+        if (type !== null && type === 'password') {
+            $('#resetpasswordform-password').attr("type", "text");
+        } else {
+            $('#resetpasswordform-password').attr("type", "password");
+        }
     });
-  });
+    
+    // Habilitacion de los popover
+    $(function () {
+        $("[data-toggle='popover']").popover({
+            trigger: 'hover'
+        });
+    });
 
-  //Utilizado para eliminar los caracteres especiales en nombres de provincias (acentos)
-  function eliminarDiacriticos(texto) {
-    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-  }
+    //Utilizado para eliminar los caracteres especiales en nombres de provincias (acentos)
+    function eliminarDiacriticos(texto) {
+        return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    }
 
-  //input provincia
-  if ($('#signupform-pais').val() == 'Argentina') {
-    autocompleteProvincias(eliminarDiacriticos('Argentina'));
-  }
-  $('#signupform-pais').change(function() {
-    autocompleteProvincias(eliminarDiacriticos($('#signupform-pais').val()));
-  });
+    //buscamos valores por defecto para pais argentina
+    if ($('#signupform-pais').val() === 'Argentina') {
+        autocompleteProvincias(eliminarDiacriticos('Argentina'));
+    };
 
-  //input localidad
-  $('#signupform-provincia').change(function() {
-    autocompleteLocalidades(eliminarDiacriticos($('#signupform-provincia').val()));
-  });
+    //input provincia
+    $('#signupform-pais').change(function () {
+        autocompleteProvincias(eliminarDiacriticos($(this).val()));
+    });
+
+    //input localidad
+    $('#signupform-provincia').change(function () {
+        autocompleteLocalidades(eliminarDiacriticos($(this).val()));
+    });
 });
+
 /**
  * Metodo autocompleteProvincia --> Busca los datos de las provincias pertenecientes al pais seleccionado
  * para ofrecer una lista de opciones de autocompletado.
@@ -63,25 +67,30 @@ $(document).ready(function() {
  * @param {String} nombrePais
  * @returns none
  */
-function autocompleteProvincias(paisName) {
-  $.ajax({
-      url: "index.php?r=site%2Fsearch-provincias&name=" + paisName,
-      type: "POST",
-      dataType: "json"
+function autocompleteProvincias(nombrePais) {
+    $.ajax({
+        url: "index.php?r=site%2Fsearch-provincias",
+        data: {pais: nombrePais},
+        type: "POST",
+        dataType: "json"
     })
-    .done(function(data) {
-      if ($("#signupform-provincia").autocomplete !== undefined) {
-        $("#signupform-provincia").autocomplete({
-          "autoFill": true,
-          "minLength": "2",
-          "source": data,
-          "select": function(event, ui) {
-            $("#signupform-provincia").val(ui.item.id);
-          }
-        });
-      }
-    });
+            .done(function (data) {
+                console.log(data);
+                if (data !== null) {
+                    if ($("#signupform-provincia").autocomplete !== undefined) {
+                        $("#signupform-provincia").autocomplete({
+                            autoFill: true,
+                            minLength: "2",
+                            source: data,
+                            select: function (event, ui) {
+                                $("#signupform-provincia").val(ui.item.id);
+                            }
+                        });
+                    }
+                }
+            });
 }
+
 /**
  * Metodo autocompleteLocalidad --> Busca los datos de las localidades pertenecientes a la provincia seleccionada
  * para ofrecer una lista de opciones de autocompletado.
@@ -89,22 +98,27 @@ function autocompleteProvincias(paisName) {
  * @param {String} nombreProvincia
  * @returns none
  */
-function autocompleteLocalidades(pronvinceName) {
-  $.ajax({
-      url: "index.php?r=site%2Fsearch-localidades&name=" + pronvinceName,
-      type: "POST",
-      dataType: "json"
+function autocompleteLocalidades(nombreProvincia) {
+    $.ajax({
+        url: "index.php?r=site%2Fsearch-localidades",
+        data: {provincia: nombreProvincia},
+        type: "POST",
+        dataType: "json"
     })
-    .done(function(data) {
-      if ($("#signupform-localidad").autocomplete !== undefined) {
-        $("#signupform-localidad").autocomplete({
-          "autoFill": true,
-          "minLength": "2",
-          "source": data,
-          "select": function(event, ui) {
-            $("#signupform-localidad").val(ui.item.id);
-          }
-        });
-      }
-    });
+            .done(function (data) {
+                console.log(data);
+                if (data !== null) {
+//                        dataLocalidades = data;
+                    if ($("#signupform-localidad").autocomplete !== undefined) {
+                        $("#signupform-localidad").autocomplete({
+                            autoFill: true,
+                            minLength: "2",
+                            source: data,
+                            select: function (event, ui) {
+                                $("#signupform-localidad").val(ui.item.id);
+                            }
+                        });
+                    }
+                }
+            });
 }
