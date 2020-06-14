9fd62ceb74c68a87176ea0adcf751a70bf59db5c
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-06-14 20:03:00 -0300

    div container

--- frontend/views/evento/cargarExpositor.php
+++ frontend/views/evento/cargarExpositor.php
@@ -12,43 +12,45 @@ use yii\widgets\ActiveForm;
 /* @var $model frontend\models\Expositor */
 
 ?>
-<div class = "row">
-    <div class = "col-md-8 col-12 m-auto">
-        <div class="evento-form">
-            <h2 class="text-center">Cargar expositor</h2>
-            <p class="text-center">Escriba el nombre del expositor</p>
+<div class="container">
+    <div class = "row">
+        <div class = "col-md-8 col-12 m-auto">
+            <div class="evento-form">
+                <h2 class="text-center">Cargar expositor</h2>
+                <p class="text-center">Escriba el nombre del expositor</p>
 
-                <div class="expositor-form">
+                    <div class="expositor-form">
 
-                    <?php $form = ActiveForm::begin(); ?>
+                        <?php $form = ActiveForm::begin(); ?>
 
-                    <?php $data = Usuario::find()
-                        ->select(["CONCAT(nombre,' ',apellido) as value", "CONCAT(nombre,' ',apellido)  as  label", "idUsuario as idUsuario"])
-                        ->asArray()
-                        ->all();
-                    ?>
+                        <?php $data = Usuario::find()
+                            ->select(["CONCAT(nombre,' ',apellido) as value", "CONCAT(nombre,' ',apellido)  as  label", "idUsuario as idUsuario"])
+                            ->asArray()
+                            ->all();
+                        ?>
 
-                    <?=
-                        'Expositor ' . '<br>' .
-                            AutoComplete::widget([
-                                'clientOptions' => [
-                                    'source' => $data,
-                                    'autoFill' => true,
-                                    'minLength' => '3',
-                                    'select' => new JsExpression("function( event, ui ) { $('#inputIdExpositor').val(ui.item.idUsuario);}")
-                                ],
-                                'options' => [
-                                    'class' => 'form-control'
-                                ]
-                            ]);
-                    ?>
-                    <?= Html::activeHiddenInput($model, 'idExpositor', ['id' => 'inputIdExpositor']) . '<br>' ?>
-                    <div class="form-group">
-                        <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
-                    </div>
+                        <?=
+                            'Expositor ' . '<br>' .
+                                AutoComplete::widget([
+                                    'clientOptions' => [
+                                        'source' => $data,
+                                        'autoFill' => true,
+                                        'minLength' => '3',
+                                        'select' => new JsExpression("function( event, ui ) { $('#inputIdExpositor').val(ui.item.idUsuario);}")
+                                    ],
+                                    'options' => [
+                                        'class' => 'form-control'
+                                    ]
+                                ]);
+                        ?>
+                        <?= Html::activeHiddenInput($model, 'idExpositor', ['id' => 'inputIdExpositor']) . '<br>' ?>
+                        <div class="form-group">
+                            <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
+                        </div>
 
-                    <?php ActiveForm::end(); ?>
-             </div>                           
-        </div>                          
+                        <?php ActiveForm::end(); ?>
+                </div>                           
+            </div>                          
+        </div>
     </div>
 </div>
