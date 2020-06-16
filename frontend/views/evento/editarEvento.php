4ecd396e3500a864b25e1dcb9d49f89eddc75690
maxi11037 <64989228+maxi11037@users.noreply.github.com>
2020-06-16 00:06:43 -0300

    Se agrego condicion segun idUsuario

--- frontend/views/evento/editarEvento.php
+++ frontend/views/evento/editarEvento.php
@@ -11,6 +11,9 @@ use frontend\models\ModalidadEvento;
 /* @var $model frontend\models\Evento */
 /* @var $form yii\widgets\ActiveForm */
 ?>
+<?php
+if(!Yii::$app->user->isGuest && Yii::$app->user->identity->idUsuario == $model->idUsuario0->idUsuario){
+?>
 <div class="container">
     <div class = "row">
         <div class = "col-md-8 col-12 m-auto">
@@ -85,3 +88,21 @@ use frontend\models\ModalidadEvento;
         </div>
     </div>    
 </div>
+<?php
+}
+else{
+	?>
+	<div class="container">
+		<div class="row">
+			<div class="col-12 text-center">
+				<div class="row">
+					<div class="col-12" style="padding-top: 4vh; padding-bottom: 4vh;">
+						<p class="display-1">403</p>
+					</div>
+				</div>
+				<p><b>Error</b>: usted no tiene permisos para gestionar este evento.</p>
+				<p>Si cree que esto es un error del servidor, contacte con un administrador del sistema</p>
+			</div>
+		</div>
+	</div>
+<?php } ?>
