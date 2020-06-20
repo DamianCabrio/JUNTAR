d81add66ec1ba5f5ddd41c2db84f425d620f1c64
maxi11037 <64989228+maxi11037@users.noreply.github.com>
2020-06-20 19:44:09 -0300

    Agregado permisos de usuario

--- frontend/views/presentacion/cargarPresentacion.php
+++ frontend/views/presentacion/cargarPresentacion.php
@@ -11,9 +11,13 @@ use yii\bootstrap4\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 <div class="container">
+
     <div class="row">
         <div class="col-md-8 col-12 m-auto">
             <div class="presentacion-form">
+			<?php
+	if(!Yii::$app->user->isGuest && Yii::$app->user->identity->idUsuario == $evento->idUsuario0->idUsuario){
+		?>
                 <h2 class="text-center">Cargar Presentacion a evento</h2>
                 <p class="text-center">Complete los siguientes campos</p>
 
@@ -62,4 +66,17 @@ use yii\bootstrap4\ActiveForm;
             </div>
         </div>
     </div>
+	<?php } else{ ?>
+		 <div class="row">
+			<div class="col-12 text-center">
+				<div class="row">
+					<div class="col-12" style="padding-top: 4vh; padding-bottom: 4vh;">
+						<p class="display-1">403</p>
+					</div>
+				</div>
+				<p><b>Error</b>: usted no tiene permisos para gestionar esta presentacion.</p>
+				<p>Si cree que esto es un error del servidor, contacte con un administrador del sistema</p>
+			</div>
+		</div>
+    <?php } ?>
 </div>    
