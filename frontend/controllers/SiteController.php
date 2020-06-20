0233276b9efb260d1399e6c6c8b0e5bd15dba97c
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-20 02:03:21 -0300

    error view fix

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -74,6 +74,21 @@ class SiteController extends Controller
 
         return $behaviors;
     }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function actions() {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
 
     /**
      * Displays homepage.
