03e78d4586b0edd108577a70c2c917cb7465b8e8
maxi11037 <64989228+maxi11037@users.noreply.github.com>
2020-06-20 19:47:02 -0300

    Update cargarExpositor.php

--- frontend/views/evento/cargarExpositor.php
+++ frontend/views/evento/cargarExpositor.php
@@ -13,6 +13,10 @@ use yii\widgets\ActiveForm;
 
 ?>
 <div class="container">
+<?php
+//echo $objetoEvento->idUsuario0->idUsuario;
+if(!Yii::$app->user->isGuest && $objetoEvento->idUsuario0->idUsuario == Yii::$app->user->identity->idUsuario){
+?>
     <div class = "row">
         <div class = "col-md-8 col-12 m-auto">
             <div class="evento-form">
@@ -53,4 +57,20 @@ use yii\widgets\ActiveForm;
             </div>                          
         </div>
     </div>
+	<?php } 
+	else{ ?>
+	<div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <div class="row">
+                    <div class="col-12" style="padding-top: 4vh; padding-bottom: 4vh;">
+                        <p class="display-1">403</p>
+                    </div>
+                </div>
+                <p><b>Error</b>: usted no tiene permisos para gestionar este evento.</p>
+                <p>Si cree que esto es un error del servidor, contacte con un administrador del sistema</p>
+            </div>
+        </div>
+    </div>
+	<?php } ?>
 </div>
