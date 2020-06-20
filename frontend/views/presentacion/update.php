d68ead05ae7f24b394c7127d0822eccf984ea28d
maxi11037 <64989228+maxi11037@users.noreply.github.com>
2020-06-20 19:45:26 -0300

    Modificar nombre de clase "presentacion-form"

--- frontend/views/presentacion/update.php
+++ frontend/views/presentacion/update.php
@@ -6,19 +6,19 @@ use yii\bootstrap4\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Presentacion */
 
-$this->title = 'Update Presentacion: ' . $model->idPresentacion;
-$this->params['breadcrumbs'][] = ['label' => 'Presentacions', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->idPresentacion, 'url' => ['view', 'id' => $model->idPresentacion]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = 'Editar Presentacion: ' . $model->idPresentacion;
+//$this->params['breadcrumbs'][] = ['label' => 'Presentacions', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = ['label' => $model->idPresentacion, 'url' => ['view', 'id' => $model->idPresentacion]];
+//$this->params['breadcrumbs'][] = 'Update';
 ?>
-<div class="presentacion-update">
+<div class="container">
+<div class="presentacion-form">
     <?php
     if (!Yii::$app->user->isGuest && Yii::$app->user->identity->idUsuario == $model->idEvento0->idUsuario0->idUsuario) {
         ?>
 
         <h1><?= Html::encode($this->title) ?></h1>
 
-        <div class="presentacion-form">
 
             <?php $form = ActiveForm::begin(); ?>
 
@@ -42,23 +42,21 @@ $this->params['breadcrumbs'][] = 'Update';
 
             <?php ActiveForm::end(); ?>
 
-        </div>
 
         <?php
     } else {
         ?>
-        <div class="container">
             <div class="row">
-                <div class="col-12 text-center presentacion-form">
+                <div class="col-12 text-center">
                     <div class="row">
                         <div class="col-12" style="padding-top: 4vh; padding-bottom: 4vh;">
-                            <p class="display-1">Error 403</p>
+                            <p class="display-1">403</p>
                         </div>
                     </div>
                     <p><b>Error</b>: usted no tiene permisos para gestionar este evento.</p>
                     <p>Si cree que esto es un error del servidor, contacte con un administrador del sistema</p>
                 </div>
             </div>
-        </div>
     <?php } ?>
+</div>
 </div>
