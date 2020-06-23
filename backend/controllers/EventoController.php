6e121d44438fbe0ca5f5e5431edb5750f2cf0e51
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-22 23:27:05 -0300

    Update: ABM Eventos
    Añadidos todos los archivos necesarios para el abm de eventos en el backend.

---
+++ backend/controllers/EventoController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use backend\models\Evento;
+use backend\models\EventoSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * EventoController implements the CRUD actions for Evento model.
+ */
+class EventoController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Evento models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new EventoSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Evento model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Evento model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Evento();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->idEvento]);
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing Evento model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->idEvento]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing Evento model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Evento model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Evento the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Evento::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
---
+++ backend/models/Evento.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "evento".
+ *
+ * @property int $idEvento
+ * @property int $idUsuario
+ * @property int $idCategoriaEvento
+ * @property int $idEstadoEvento
+ * @property int $idModalidadEvento
+ * @property string $nombreEvento
+ * @property string $nombreCortoEvento
+ * @property string $descripcionEvento
+ * @property string $lugar
+ * @property string $fechaInicioEvento
+ * @property string $fechaFinEvento
+ * @property string|null $imgFlyer
+ * @property string|null $imgLogo
+ * @property int $capacidad
+ * @property int $preInscripcion
+ * @property string|null $fechaLimiteInscripcion
+ * @property string|null $codigoAcreditacion
+ * @property string|null $fechaCreacionEvento
+ *
+ * @property CategoriaEvento $idCategoriaEvento0
+ * @property EstadoEvento $idEstadoEvento0
+ * @property ModalidadEvento $idModalidadEvento0
+ * @property Usuario $idUsuario0
+ * @property Inscripcion[] $inscripcions
+ * @property Presentacion[] $presentacions
+ */
+class Evento extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'evento';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['idUsuario', 'idCategoriaEvento', 'idEstadoEvento', 'idModalidadEvento', 'nombreEvento', 'nombreCortoEvento', 'descripcionEvento', 'lugar', 'fechaInicioEvento', 'fechaFinEvento', 'capacidad', 'preInscripcion'], 'required'],
+            [['idUsuario', 'idCategoriaEvento', 'idEstadoEvento', 'idModalidadEvento', 'capacidad', 'preInscripcion'], 'integer'],
+            [['fechaInicioEvento', 'fechaFinEvento', 'fechaLimiteInscripcion', 'fechaCreacionEvento'], 'safe'],
+            [['nombreEvento', 'lugar', 'imgFlyer', 'imgLogo'], 'string', 'max' => 200],
+            [['nombreCortoEvento', 'codigoAcreditacion'], 'string', 'max' => 100],
+            [['descripcionEvento'], 'string', 'max' => 800],
+            [['idUsuario'], 'exist', 'skipOnError' => true, 'targetClass' => Usuario::className(), 'targetAttribute' => ['idUsuario' => 'idUsuario']],
+            [['idCategoriaEvento'], 'exist', 'skipOnError' => true, 'targetClass' => CategoriaEvento::className(), 'targetAttribute' => ['idCategoriaEvento' => 'idCategoriaEvento']],
+            [['idModalidadEvento'], 'exist', 'skipOnError' => true, 'targetClass' => ModalidadEvento::className(), 'targetAttribute' => ['idModalidadEvento' => 'idModalidadEvento']],
+            [['idEstadoEvento'], 'exist', 'skipOnError' => true, 'targetClass' => EstadoEvento::className(), 'targetAttribute' => ['idEstadoEvento' => 'idEstadoEvento']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'idEvento' => 'Id Evento',
+            'idUsuario' => 'Id Usuario',
+            'idCategoriaEvento' => 'Id Categoria Evento',
+            'idEstadoEvento' => 'Id Estado Evento',
+            'idModalidadEvento' => 'Id Modalidad Evento',
+            'nombreEvento' => 'Nombre Evento',
+            'nombreCortoEvento' => 'Nombre Corto Evento',
+            'descripcionEvento' => 'Descripcion Evento',
+            'lugar' => 'Lugar',
+            'fechaInicioEvento' => 'Fecha Inicio Evento',
+            'fechaFinEvento' => 'Fecha Fin Evento',
+            'imgFlyer' => 'Img Flyer',
+            'imgLogo' => 'Img Logo',
+            'capacidad' => 'Capacidad',
+            'preInscripcion' => 'Pre Inscripcion',
+            'fechaLimiteInscripcion' => 'Fecha Limite Inscripcion',
+            'codigoAcreditacion' => 'Codigo Acreditacion',
+            'fechaCreacionEvento' => 'Fecha Creacion Evento',
+        ];
+    }
+
+    /**
+     * Gets query for [[IdCategoriaEvento0]].
+     *
+     * @return \yii\db\ActiveQuery|CategoriaEventoQuery
+     */
+    public function getIdCategoriaEvento0()
+    {
+        return $this->hasOne(CategoriaEvento::className(), ['idCategoriaEvento' => 'idCategoriaEvento']);
+    }
+
+    /**
+     * Gets query for [[IdEstadoEvento0]].
+     *
+     * @return \yii\db\ActiveQuery|EstadoEventoQuery
+     */
+    public function getIdEstadoEvento0()
+    {
+        return $this->hasOne(EstadoEvento::className(), ['idEstadoEvento' => 'idEstadoEvento']);
+    }
+
+    /**
+     * Gets query for [[IdModalidadEvento0]].
+     *
+     * @return \yii\db\ActiveQuery|ModalidadEventoQuery
+     */
+    public function getIdModalidadEvento0()
+    {
+        return $this->hasOne(ModalidadEvento::className(), ['idModalidadEvento' => 'idModalidadEvento']);
+    }
+
+    /**
+     * Gets query for [[IdUsuario0]].
+     *
+     * @return \yii\db\ActiveQuery|UsuarioQuery
+     */
+    public function getIdUsuario0()
+    {
+        return $this->hasOne(Usuario::className(), ['idUsuario' => 'idUsuario']);
+    }
+
+    /**
+     * Gets query for [[Inscripcions]].
+     *
+     * @return \yii\db\ActiveQuery|InscripcionQuery
+     */
+    public function getInscripcions()
+    {
+        return $this->hasMany(Inscripcion::className(), ['idEvento' => 'idEvento']);
+    }
+
+    /**
+     * Gets query for [[Presentacions]].
+     *
+     * @return \yii\db\ActiveQuery|PresentacionQuery
+     */
+    public function getPresentacions()
+    {
+        return $this->hasMany(Presentacion::className(), ['idEvento' => 'idEvento']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return EventoQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new EventoQuery(get_called_class());
+    }
+}
