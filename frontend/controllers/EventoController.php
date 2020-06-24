9a96c58e35291baf273cac4c8ca2d938f8740228
Damian Cabrio <45396062+DamianCabrio@users.noreply.github.com>
2020-06-24 16:38:36 -0300

    Arreglo logia de estados de botones

--- frontend/controllers/EventoController.php
+++ frontend/controllers/EventoController.php
@@ -96,14 +96,18 @@ class EventoController extends Controller {
     }
 
     public function obtenerEstadoEventoNoLogin($cupos, $evento) {
-        if((strtotime($evento->fechaLimiteInscripcion) > date("Y-m-d") && $evento->fechaLimiteInscripcion != null) || strtotime($evento->fechaInicioEvento) > date("Y-m-d")){
-            return "noInscriptoYFechaLimiteInscripcionPasada";
-        }else{
+        if(($evento->fechaLimiteInscripcion != null && strtotime($evento->fechaLimiteInscripcion) > date("Y-m-d")) ||
+            strtotime($evento->fechaInicioEvento) > date("Y-m-d")){
             if ($cupos !== 0 || is_null($cupos)) {
                 return $evento->preInscripcion == 0 ? "puedeInscripcion" : "puedePreinscripcion";
             } else {
                 return "sinCupos";
             }
+        }elseif ($evento->fechaLimiteInscripcion == null){
+            return $evento->preInscripcion == 0 ? "puedeInscripcion" : "puedePreinscripcion";
+        }
+        else{
+            return "noInscriptoYFechaLimiteInscripcionPasada";
         }
     }
 
