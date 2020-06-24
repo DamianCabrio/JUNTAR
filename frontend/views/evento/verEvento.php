f652bec6778109f93ff97c14938f54c6e0dbf10d
n-stange <64751066+n-stange@users.noreply.github.com>
2020-06-24 19:55:51 -0300

    Remove del segundo botón suspender/publicar que apreció en un merge en vista evento :o

--- frontend/views/evento/verEvento.php
+++ frontend/views/evento/verEvento.php
@@ -125,22 +125,6 @@ $organizadorEmailEvento = $evento->idUsuario0->email;
                         echo '<div class="card-header pinkish_bg"> <br> </div>';
                     }
                     ?>
-                    <?php
-                    if (!Yii::$app->user->isGuest && $evento->idUsuario == Yii::$app->user->identity->idUsuario) {
-                        if (($evento->idEstadoEvento) == 4) {
-                            ?>
-                                <?= Html::a('Publicar', ['eventos/publicar-evento/' . $evento->nombreCortoEvento], ['class' => 'btn btn-outline-success']) ?>
-                            <?php } ?>
-
-                            <?php
-                            if (($evento->idEstadoEvento) == 1) {
-                            ?>
-                                <?= Html::a('Suspender', ['eventos/suspender-evento/' . $evento->nombreCortoEvento], ['class' => 'btn btn-outline-danger']) ?>
-                            <?php
-                            }
-                        }
-                    
-                    ?>
 
                     <div class="card-body">
                         <div class="row padding_section">
