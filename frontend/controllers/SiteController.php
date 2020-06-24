0fa9a9394875f0decf4583b40541489298ca5fdd
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-23 23:47:15 -0300

    Actualizacion usuario: signup error
    Solucionado error de registro en el sitio: cambiado el orden de validar y registrar para que no haya conflictos. Eliminado password como requisito para modificar datos de usuario, y eliminada variable privada en desuso del modelo user.

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -257,7 +257,7 @@ class SiteController extends Controller
         $paises = ArrayHelper::map($paises['countries'], 'id', 'name');
         $paises = $this->conversionAutocomplete($paises);
         $model = new SignupForm();
-        if ($model->load(Yii::$app->request->post()) && $model->signup() && $model->validate()) {
+        if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->signup() ) {
             Yii::$app->session->setFlash('success', '<h2> ¡Sólo queda confirmar tu correo! </h2>'
                     . '<p> Muchas gracias por registrarte en la plataforma Juntar. Por favor, revisa tu dirección de correo para confirmar tu cuenta. </p>');
             return $this->goBack(Url::previous());
