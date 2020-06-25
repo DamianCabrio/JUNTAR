ef317f28065956c7a0750087359159316f302278
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-25 14:48:41 -0300

    Update email: background color
    Se corrigió color de fondo que genera conflictos en diferentes plataformas

--- common/mail/emailVerify-html.php
+++ common/mail/emailVerify-html.php
@@ -15,7 +15,7 @@ $verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'to
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
-<body style="background-color: #0b0d19;">
+<body>
     <style>
         .link-button {
             border: 1px solid #ccc;
