04ef82f759cad699c493697aa8163ca98849eea6
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-24 23:55:30 -0300

    Update aliases: QRCodes e ImagenPerfil
    Fueron agregados los alias para trabajar con codigos QR e imagen de perfil

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -16,6 +16,8 @@ return [
     'aliases' =>[
         '@rutaLogo' => '/eventos/images/logos/',
         '@rutaFlyer' => '/eventos/images/flyers/',
+        '@rutaQR' => '/eventos/images/qrcodes/',
+        '@rutaImagenPerfil' => '/profile/images/',
     ],
     'components' => [
         'qr' => [
