2d871462ff7ccfdb479441e38ccdab89b26cb82a
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-06-26 12:09:45 -0300

    Se quitaron lineas  de codigo cometado

--- frontend/views/presentacion/view.php
+++ frontend/views/presentacion/view.php
@@ -7,30 +7,12 @@ use yii\widgets\DetailView;
 /* @var $model frontend\models\Presentacion */
 
 $this->title = $model->tituloPresentacion;
-//$this->params['breadcrumbs'][] = ['label' => 'Presentacions', 'url' => ['index']];
-//$this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="container">
 <div class="presentacion-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
-	<?php
-	/*
-	if(!Yii::$app->user->isGuest && Yii::$app->user->identity->idUsuario == $model->idEvento0->idUsuario0->idUsuario){
-		?>
-    <p>
-        <?= Html::a('Actualizar', ['update', 'id' => $model->idPresentacion], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Borrar', ['delete', 'id' => $model->idPresentacion], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Estás seguro de querer borrar esta presentación?',
-                'method' => 'post',
-            ],
-        ]) ?>
-	
-    </p>
-	<?php } */?>
 
     <?= DetailView::widget([
         'model' => $model,
