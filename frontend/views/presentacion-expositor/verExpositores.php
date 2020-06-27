dd00b3a5055f3d97653b241008c5cf281ddf4da0
maxi11037 <64989228+maxi11037@users.noreply.github.com>
2020-06-26 21:41:37 -0300

    Agregado: modal dentro de modal para eliminar expositor

--- frontend/views/presentacion-expositor/verExpositores.php
+++ frontend/views/presentacion-expositor/verExpositores.php
@@ -55,16 +55,37 @@ use yii\bootstrap4\Modal;
                         //genera una url para cada evento
                         'urlCreator' => function ($action, $model, $key, $index) {
                             if ($action == "delete") {
-                                return Url::to(['presentacion-expositor/delete', 'idPresentacion' => $key['idPresentacion'], 'idExpositor' => $key['idExpositor']]);
+								?>
+								<div class="modal fade" id="myModal" style="background:#00000082;">
+									<div class="modal-dialog" style="margin-top: 5rem;">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h4 class="modal-title"></h4>
+											</div>
+											<div class="container"></div>
+											<div class="modal-body">
+												<p>Está seguro de querer borrar este expositor?</p>
+											</div>
+											<div class="modal-footer">	
+												<a href="<?= Url::to(['presentacion-expositor/delete', 'idPresentacion' => $key['idPresentacion'], 'idExpositor' => $key['idExpositor']]); ?>" class="btn btn-primary">Si</a>
+												<a href="#" data-dismiss="modal" class="btn">No</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<?php
+								
+							//return Url::to(['presentacion-expositor/delete', 'idPresentacion' => $key['idPresentacion'], 'idExpositor' => $key['idExpositor']]);
+							//return Url::to(['#']);
                             }
                         },
                         //describe los botones de accion
                         'buttons' => [
                             'delete' => function ($url, $model) {
-                                return Html::a('<i class="material-icons large align-middle">remove_circle_outline</i>', $url, ['class' => 'btn borrarPresentacion', 'data' => [
+                                return Html::a('<i class="material-icons large align-middle">remove_circle_outline</i>', $url, ['class' => 'btn verPresentacion', 'data-toggle' => 'modal', 'href' => '#myModal' /*'data' => [
                                                 'confirm' => 'Esta seguro que desea Borrar el Expositor?',
                                                 'method' => 'post',
-                                            ],]);
+                                            ]*/]);
                             },
                         ],
                         'visible' => !Yii::$app->user->isGuest && $model->idEvento0->idUsuario == Yii::$app->user->identity->idUsuario && ($evento->idEstadoEvento == 1 || $evento->idEstadoEvento == 4 || $evento->idEstadoEvento == 3),
@@ -84,4 +105,6 @@ use yii\bootstrap4\Modal;
 
         </div>
     </div>
+	
+	
 </div>
