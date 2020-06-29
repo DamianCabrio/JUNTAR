325fec0ff83d0b44cfdb9b939a6f188df79d230a
Lean <leandro.casanova@est.fi.uncoma.edu.ar>
2020-06-28 22:21:50 -0300

    Apariencia del Backend - layouts

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -27,6 +27,31 @@ AppAsset::register($this);
         <?php $this->beginBody() ?>
 
         <div class="wrap">
+          <?php if (Yii::$app->requestedRoute == 'site/index' || Yii::$app->requestedRoute == null ): ?>
+            <div class="row">
+              <div class="col-12 text-center">
+                <?= Html::img('images/juntar-logo/svg/juntar-logo-b.svg',  [
+                  'class' => 'img-fluid mx-auto d-block',
+                  'style' => ['width' => '25%']
+                  ])?>
+                  <h3 class="mt-2">Aplicación Backend</h3>
+              </div>
+            </div>
+            <?php
+            $menuItems[] = [
+                "label" => "Salir (" . Yii::$app->user->identity->nombre ." ". Yii::$app->user->identity->apellido .")",
+                "url" => ["/site/logout"],
+                "linkOptions" => [
+                    "data-method" => "post",
+                ]
+            ];
+
+            echo Nav::widget([
+              'options' => ['class' => 'navbar-nav navbar-collapse justify-content-end'],
+              'items' => $menuItems,
+            ]);
+             ?>
+          <?php else: ?>
             <?php
             NavBar::begin([
                 'brandLabel' => Html::img('@web/images/juntar-logo/svg/juntar-icon-w.svg', ['style' => 'width:30px']),
@@ -86,7 +111,7 @@ AppAsset::register($this);
             ]);
             NavBar::end();
             ?>
-
+            <?php endif; ?>
 
             <div class="container">
                 <?=
@@ -101,8 +126,48 @@ AppAsset::register($this);
             </div>
         </div>
 
-        <footer class="footer fixed-bottom">
-            <!--<footer class="footer">-->
+        <section class="darkish_bg text-light">
+            <div class="container" style="padding-bottom: 4vh;">
+                <div class="row">
+                    <div class="col-12 col-md-5" style="padding-top: 4vh; padding-bottom: 4vh;">
+                        <?= Html::img('images/juntar-logo/svg/juntar-logo-w.svg',  ['class' => 'img-fluid']); ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-8">
+                        <h5 class="white-text">Juntar</h5>
+
+                        <p class="grey-text text-lighten-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada vitae erat a lobortis. Donec pretium tortor eros, a auctor massa suscipit vel. Donec augue odio, vulputate et egestas fringilla, varius vel ligula. Aliquam eu sagittis nisi, vitae imperdiet lorem. Vivamus lobortis diam vel sapien aliquet, dictum placerat tellus imperdiet.</p>
+
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <h5 class="white-text">Contacto</h5>
+                        <ul>
+                            <li>
+                                <a class="white-text link" href="#!">juntar@fi.uncoma.edu.ar</a>
+                            </li>
+                        </ul>
+                        <h5 class="white-text">Sobre</h5>
+                    <ul>
+                        <li>
+                            <?= Html::a('Sobre', ['site/about'], ['class' => 'profile-link']) ?>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                <hr>
+                <div class="row" style="padding-top: 4vh;padding-bottom: 2vh;">
+                    <div class="col-12 col-md-6 py-3">
+                        <?= Html::img('images/uncoma.png',  ['class' => 'img-fluid']); ?>
+                    </div>
+                    <div class="col-12 col-md-6 py-3">
+                        <?= Html::img('images/fai.png',  ['class' => 'img-fluid']); ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer class="footer dark_bg text-light">
+
             <div class="container">
                 <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
             </div>
