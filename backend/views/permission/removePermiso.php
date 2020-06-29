3c352bef55428f21906c2a2677c8a209767a6767
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-28 22:18:13 -0300

    Backend update: permiso
    Actualización de estilo para las vistas de permiso en el backend.

--- backend/views/permission/removePermiso.php
+++ backend/views/permission/removePermiso.php
@@ -12,26 +12,29 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="row">
     <div class="col-12 mb-4">
-        <h1 class="card-header text-center darkish_bg text-white"> <?= Html::encode($this->title) ?> </h1>
-    </div>
-    <div class="col">
-        <div class="rol-create">
+        <div class="card">
+            <h1 class="card-header text-center darkish_bg text-white"> <?= Html::encode($this->title) ?> </h1>
 
-            <?php $form = ActiveForm::begin(); ?>
+            <div class="card-body col">
+                <div class="rol-create">
 
-            <?= $form->field($model, 'name')->dropdownlist($item, ['class' => 'form-control', 'prompt' => 'Seleccione un permiso...']); ?>
+                    <?php $form = ActiveForm::begin(); ?>
 
-            <div class="form-group">
-                <?=
-                    Html::submitButton('Eliminar', [
-                        'class' => 'btn btn-danger',
-                        'data' => [
-                            'confirm' => '¿Está seguro de querer eliminar el Permiso?'
-                        ],
-                    ])
-                ?>
+                    <?= $form->field($model, 'name')->dropdownlist($item, ['class' => 'form-control', 'prompt' => 'Seleccione un permiso...']); ?>
+
+                    <div class="form-group">
+                        <?=
+                        Html::submitButton('Eliminar', [
+                            'class' => 'btn btn-danger col-2',
+                            'data' => [
+                                'confirm' => '¿Está seguro de querer eliminar el Permiso?'
+                            ],
+                        ])
+                        ?>
+                    </div>
+                    <?php ActiveForm::end(); ?>
+                </div>
             </div>
-            <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
--- backend/views/permission/verPermiso.php
+++ backend/views/permission/verPermiso.php
@@ -12,46 +12,54 @@ $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="permiso-view">
+    <div class="card">
 
-    <h1> Permiso: <?= Html::encode($this->title) ?> </h1>
+        <h1 class="card-header text-center darkish_bg text-white"> Permiso: <?= Html::encode($this->title) ?> </h1>
 
-    <p>
-        <?php // echo Html::a('Update', ['update-permiso', 'name' => $model->name], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['/permission/remove-permiso', 'name' => $model->name], [
-            'class' => 'btn btn-danger',
+        <div class="card-body">
+            <p>
+                <?php // echo Html::a('Update', ['update-permiso', 'name' => $model->name], ['class' => 'btn btn-primary']) ?>
+                <?=
+                Html::a('Delete', ['/permission/remove-permiso', 'name' => $model->name], [
+                    'class' => 'btn btn-danger col-2',
 //            'data' => [
 //                'confirm' => '¿Está seguro que desea eliminar este permiso?',
 //            ],
-        ]) ?>
-    </p>
+                ])
+                ?>
+            </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'name',
-            [
-                'attribute' => 'type',
-                'label' => 'Tipo',
-                'value' => ($model->type == 1)? 'Rol':'Permiso', //valor referenciado
-            ],
-            [
-                'attribute' => 'description',
-                'label' => 'Descripcion',
-                'value' => $model->description, //valor referenciado
-            ],
-            [
-                'attribute' => 'created_at',
-                'label' => 'Fecha Creación',
-                'value' => date("Y-m-d H:i:s", $model->created_at), //valor referenciado
-            ],
-            [
-                'attribute' => 'updated_at',
-                'label' => 'Fecha Actualización',
-                'value' => date("Y-m-d H:i:s", $model->updated_at), //valor referenciado
-            ],
+            <?=
+            DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'name',
+                    [
+                        'attribute' => 'type',
+                        'label' => 'Tipo',
+                        'value' => ($model->type == 1) ? 'Rol' : 'Permiso', //valor referenciado
+                    ],
+                    [
+                        'attribute' => 'description',
+                        'label' => 'Descripcion',
+                        'value' => $model->description, //valor referenciado
+                    ],
+                    [
+                        'attribute' => 'created_at',
+                        'label' => 'Fecha Creación',
+                        'value' => date("Y-m-d H:i:s", $model->created_at), //valor referenciado
+                    ],
+                    [
+                        'attribute' => 'updated_at',
+                        'label' => 'Fecha Actualización',
+                        'value' => date("Y-m-d H:i:s", $model->updated_at), //valor referenciado
+                    ],
 //            'created_at',
 //            'updated_at',
-        ],
-    ]) ?>
+                ],
+            ])
+            ?>
+        </div>
+    </div>
 
 </div>
