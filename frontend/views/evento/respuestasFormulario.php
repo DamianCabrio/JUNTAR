08f6dfd86a93c9e77d51d06e9187a944d12728c0
n-stange <64751066+n-stange@users.noreply.github.com>
2020-06-28 23:02:39 -0300

    Update respuestas-formulario
    Unificación con el diseño general

--- frontend/views/evento/respuestasFormulario.php
+++ frontend/views/evento/respuestasFormulario.php
@@ -10,77 +10,93 @@ $this->title = "Respuesta a el formulario";
 
 <div class="respuestas container">
 
-    <h3>Usuarios preinscriptos</h3>
-    <?=
-    \yii\grid\GridView::widget([
-        "dataProvider" => $preinscriptos,
-        'summary' => '',
-        'options' => ['style' => 'width:100%;'],
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            [
-                'attribute' => 'Nombre del usuario',
-                'format' => 'raw',
-                'value' => function ($dataProvider) {
-                    return $dataProvider->idUsuario0->nombre . " " . $dataProvider->idUsuario0->apellido;
-                },
-                'headerOptions' => ['style' => 'width:30%;text-align:center;'],
-            ],
-            [
-                'attribute' => 'Acciones',
-                'format' => 'raw',
-                'value' => function ($dataProvider) use($hayPreguntas) {
-                    if($hayPreguntas){
-                        $return = Html::a("Ver respuestas", Url::toRoute("respuesta/ver?id=". $dataProvider->idEvento . "&id2=". $dataProvider->idInscripcion), ["class" => "verRespuesta ml-2 btn btn-outline-success mb-2"]);
-                        $return .= Html::a("Inscribir A Evento", Url::toRoute("inscripcion/inscribir-a-usuario?idUsuario=". $dataProvider->idUsuario . "&idEvento=". $dataProvider->idEvento), ['class' => 'ml-2 btn btn-outline-success mb-2']);
-                    }else{
-                        $return = "No hay preguntas que responder en el evento";
-                    }
-                    return $return;
-                },
-                'headerOptions' => ['style' => 'width:65%;text-align:center;', 'class' => 'text-center'],
-                'contentOptions' => ['style' => 'text-align:center; vertical-align:middle;']
-            ],
-        ]
-    ]);
-    ?>
-    <h3>Usuarios inscriptos</h3>
-    <?=
-    \yii\grid\GridView::widget([
-        "dataProvider" => $inscriptos,
-        'summary' => '',
-        'options' => ['style' => 'width:100%;'],
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            [
-                'attribute' => 'Nombre del usuario',
-                'format' => 'raw',
-                'value' => function ($dataProvider) {
-                    return $dataProvider->idUsuario0->nombre . " " . $dataProvider->idUsuario0->apellido;
-                },
-                'headerOptions' => ['style' => 'width:30%;text-align:center;'],
-            ],
-            [
-                'attribute' => 'Acciones',
-                'format' => 'raw',
-                'value' => function ($dataProvider) {
-                        $return = Html::a("Ver respuestas", Url::toRoute("respuesta/ver?id=". $dataProvider->idEvento . "&id2=". $dataProvider->idInscripcion), ["class" => "verRespuesta ml-2 btn btn-outline-success mb-2"]);
-                        $return .= Html::a("Anular Inscripcion", Url::toRoute("inscripcion/anular-inscripcion?idUsuario=". $dataProvider->idUsuario . "&idEvento=". $dataProvider->idEvento), ['class' => 'ml-2 btn btn-outline-success mb-2']);
-                    return $return;
-                },
-                'headerOptions' => ['style' => 'width:65%;text-align:center;', 'class' => 'text-center'],
-                'contentOptions' => ['style' => 'text-align:center; vertical-align:middle;']
-            ],
-        ]
-    ]);
-    ?>
 
-    <?= Html::a('Volver Atrás', Url::toRoute("eventos/ver-evento/" . $evento->nombreCortoEvento), ['class' => 'btn btn-outline-success']); ?>
+    <div class="card mb-4">
+        <div class="card-header dark_bg text-light">
+            <h3>Usuarios Preinscriptos</h3>
+        </div>
+        <div class="card-body">
+            <?=
+                \yii\grid\GridView::widget([
+                    "dataProvider" => $preinscriptos,
+                    'summary' => '',
+                    'options' => ['style' => 'width:100%;'],
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+                        [
+                            'attribute' => 'Nombre del usuario',
+                            'format' => 'raw',
+                            'value' => function ($dataProvider) {
+                                return $dataProvider->idUsuario0->nombre . " " . $dataProvider->idUsuario0->apellido;
+                            },
+                            'headerOptions' => ['style' => 'width:30%;text-align:center;'],
+                        ],
+                        [
+                            'attribute' => 'Acciones',
+                            'format' => 'raw',
+                            'value' => function ($dataProvider) use ($hayPreguntas) {
+                                if ($hayPreguntas) {
+                                    $return = Html::a("Ver respuestas", Url::toRoute("respuesta/ver?id=" . $dataProvider->idEvento . "&id2=" . $dataProvider->idInscripcion), ["class" => "verRespuesta ml-2 btn btn-outline-success mb-2"]);
+                                    $return .= Html::a("Inscribir A Evento", Url::toRoute("inscripcion/inscribir-a-usuario?idUsuario=" . $dataProvider->idUsuario . "&idEvento=" . $dataProvider->idEvento), ['class' => 'ml-2 btn btn-outline-success mb-2']);
+                                } else {
+                                    $return = "No hay preguntas que responder en el evento";
+                                }
+                                return $return;
+                            },
+                            'headerOptions' => ['style' => 'width:65%;text-align:center;', 'class' => 'text-center'],
+                            'contentOptions' => ['style' => 'text-align:center; vertical-align:middle;']
+                        ],
+                    ]
+                ]);
+            ?>
+        </div>
+    </div>
+    <div class="card mb-4">
+        <div class="card-header dark_bg text-light">
+        <h3>Usuarios Inscriptos</h3>
+        </div>
+        <div class="card-body">
+        <?=
+        \yii\grid\GridView::widget([
+            "dataProvider" => $inscriptos,
+            'summary' => '',
+            'options' => ['style' => 'width:100%;'],
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                [
+                    'attribute' => 'Nombre del usuario',
+                    'format' => 'raw',
+                    'value' => function ($dataProvider) {
+                        return $dataProvider->idUsuario0->nombre . " " . $dataProvider->idUsuario0->apellido;
+                    },
+                    'headerOptions' => ['style' => 'width:30%;text-align:center;'],
+                ],
+                [
+                    'attribute' => 'Acciones',
+                    'format' => 'raw',
+                    'value' => function ($dataProvider) {
+                        $return = Html::a("Ver respuestas", Url::toRoute("respuesta/ver?id=" . $dataProvider->idEvento . "&id2=" . $dataProvider->idInscripcion), ["class" => "verRespuesta ml-2 btn btn-outline-success mb-2"]);
+                        $return .= Html::a("Anular Inscripcion", Url::toRoute("inscripcion/anular-inscripcion?idUsuario=" . $dataProvider->idUsuario . "&idEvento=" . $dataProvider->idEvento), ['class' => 'ml-2 btn btn-outline-success mb-2']);
+                        return $return;
+                    },
+                    'headerOptions' => ['style' => 'width:65%;text-align:center;', 'class' => 'text-center'],
+                    'contentOptions' => ['style' => 'text-align:center; vertical-align:middle;']
+                ],
+            ]
+        ]);
+    ?>
+        </div>
+    </div>
+    
+
+
+    <?= Html::a('Volver Atrás', Url::toRoute("eventos/ver-evento/" . $evento->nombreCortoEvento), ['class' => 'btn btn-lg']); ?>
 
     <?php
     Modal::begin([
         'id' => 'modalRespuestas',
         'size' => 'modal-lg',
+        'headerOptions' => ['class' => 'dark_bg text-light']
     ]);
     Modal::end();
     ?>
