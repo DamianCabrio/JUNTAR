c24cceb3145f118b1e0d263ac1692a74623b04d0
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-06-29 03:38:06 -0300

    Backend update: Modalidades
    Actualizado el backend para permitir el abm de modalidades

--- backend/views/modalidad-evento/index.php
+++ backend/views/modalidad-evento/index.php
@@ -11,45 +11,45 @@ $this->title = 'Modalidades de los Eventos';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="modalidad-evento-index container">
-  <div class="row">
-      <div class="col-12 mb-4">
-          <h1 class="card-header text-center darkish_bg text-white"> <?= Html::encode($this->title) ?> </h1>
-      </div>
-  </div>
-  <p>
-      <?= Html::a('Crear Evento', ['create'], ['class' => 'btn btn-success']) ?>
-  </p>
-<div class="card">
-      <div class="card-body">
-          <div class="table-responsive">
-              <?= GridView::widget([
-                  'dataProvider' => $dataProvider,
-                  'filterModel' => $searchModel,
-                  'columns' => [
-                      ['class' => 'yii\grid\SerialColumn'],
-                      [
-                        'attribute' => 'descripcionModalidad',
-                        'options' => ['class' => 'col-2']
-                      ],
+    <div class="row">
+        <div class="col-12 mb-4 p-0">
+            <div class="card">
+                <h1 class="card-header text-center darkish_bg text-white"> <?= Html::encode($this->title) ?> </h1>
 
-                      ['class' => 'yii\grid\ActionColumn',
-
-                          'buttons' => [
-                              'update' => function($url, $model) {
-                                  return Html::a('<img src="'.Yii::getAlias('@web/iconos/pencil.svg').'" alt="Editar" width="20" height="20" title="Editar" role="img">', $url, ['class' => 'btn']);
-                              },
-                              'view' => function($url, $model) {
-                                  return Html::a('<img src="'.Yii::getAlias('@web/iconos/eye.svg').'" alt="Visualizar" width="20" height="20" title="Visualizar" role="img">', $url, ['class' => 'btn']);
-                              },
-                              'delete' => function($url, $model) {
-                                  return Html::a('<img src="'.Yii::getAlias('@web/iconos/trash.svg').'" alt="Borrar" width="20" height="20" title="Borrar" role="img">', $url, ['class' => 'btn']);
-                              }
-                          ]
-                      ],
-
-                  ],
-              ]); ?>
+                <p class="m-3">
+                    <?= Html::a('Crear Modalidad', ['create'], ['class' => 'btn btn-pink col-md-2 col-sm-12']) ?>
+                </p>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <?=
+                        GridView::widget([
+                            'dataProvider' => $dataProvider,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
+                                [
+                                    'attribute' => 'descripcionModalidad',
+                                    'options' => ['class' => 'col-2']
+                                ],
+                                ['class' => 'yii\grid\ActionColumn',
+                                    'buttons' => [
+                                        'update' => function($url, $model) {
+                                            return Html::a('<img class="filter-white" src="' . Yii::getAlias('@web/iconos/pencil.svg') . '" alt="Editar" width="20" height="20" title="Editar" role="img">', $url, ['class' => 'btn btn-pink']);
+                                        },
+                                        'view' => function($url, $model) {
+                                            return Html::a('<img class="filter-white" src="' . Yii::getAlias('@web/iconos/eye.svg') . '" alt="Visualizar" width="20" height="20" title="Visualizar" role="img">', $url, ['class' => 'btn btn-pink']);
+                                        },
+                                        'delete' => function($url, $model) {
+                                            return Html::a('<img class="filter-white" src="' . Yii::getAlias('@web/iconos/trash.svg') . '" alt="Borrar" width="20" height="20" title="Borrar" role="img">', $url, ['class' => 'btn btn-pink']);
+                                        }
+                                    ]
+                                ],
+                            ],
+                        ]);
+                        ?>
+                    </div>
+                </div>
+            </div>
         </div>
-     </div>
-  </div>
+    </div>
 </div>
---
+++ backend/views/modalidad-evento/view.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\ModalidadEvento */
+
+$this->title = $model->descripcionModalidad;
+$this->params['breadcrumbs'][] = ['label' => 'Modalidad Eventos', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="modalidad-evento-view">
+    <div class="col-12 mb-4 p-0">
+        <div class="card">
+            <h1 class="card-header text-center darkish_bg text-white"><?= Html::encode($this->title) ?></h1>
+
+            <p class="m-3">
+                <?= Html::a('Actualizar', ['update', 'id' => $model->idModalidadEvento], ['class' => 'btn btn-pink col-md-2 col-sm-12']) ?>
+                <?=
+                Html::a('Borrar', ['delete', 'id' => $model->idModalidadEvento], [
+                    'class' => 'btn btn-danger col-md-2 col-sm-12',
+                    'data' => [
+                        'confirm' => '¿Está seguro de querer borrar esta modalidad?',
+//                'method' => 'post',
+                    ],
+                ])
+                ?>
+            </p>
+
+            <div class="card-body">
+                <?=
+                DetailView::widget([
+                    'model' => $model,
+                    'attributes' => [
+                        'idModalidadEvento',
+                        'descripcionModalidad',
+                    ],
+                ])
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
