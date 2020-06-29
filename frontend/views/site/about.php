edb5aeed2f66d0cca60aececfaf2a09baa9d2d14
maxi11037 <64989228+maxi11037@users.noreply.github.com>
2020-06-28 23:06:50 -0300

    Update about.php

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -10,8 +10,17 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about container">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p><br>Equipo de desarrollo <br> 
-    	<br>
-    Damian Cabrio, Felipe Bastidas, Leandro Casanova, Norbert Stange, Emanuel Araya, Laura Murillo, Marcos Benitez, Mauro Saracini, Maximiliano Bajamon y Kevin Espinoza.-</p>
-	
+    <p><br>Equipo de desarrollo </p>
+		<ul>
+			<li>Damian Cabrio</li>
+			<li>Felipe Bastidas</li>
+			<li>Leandro Casanova</li>
+			<li>Norbert Stange</li>
+			<li>Emanuel Araya</li>
+			<li>Laura Murillo</li>
+			<li>Marcos Benitez</li>
+			<li>Mauro Saracini</li>
+			<li>Maximiliano Bajamon</li>
+			<li>Kevin Espinoza</li>
+		</ul>
 </div>
