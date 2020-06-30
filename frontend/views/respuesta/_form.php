29c383feb429acfa0f92491526e7f48f4e1ea5fa
maxi11037 <64989228+maxi11037@users.noreply.github.com>
2020-06-29 22:48:39 -0300

    Update: wysiwyg en respuesta larga

--- frontend/views/respuesta/_form.php
+++ frontend/views/respuesta/_form.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
 use yii\helpers\Url;
+use dosamigos\ckeditor\CKEditor;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\RespuestaFile */
@@ -19,7 +20,30 @@ use yii\helpers\Url;
     <?= $form->field($model, 'respuesta')->textInput(['maxlength' => true])->label(false) ?>
     <?php endif; ?>
     <?php if($pregunta->tipo == 2): ?>
-        <?= $form->field($model, 'respuesta')->textarea(['maxlength' => true])->label(false) ?>
+        //echo $form->field($model, 'respuesta')->textarea(['maxlength' => true])->label(false);
+        <?= $form->field($model, 'respuesta')->widget(CKEditor::className(), [
+                    "options" => ['rows' => '8'],
+                    "preset" => "custom",
+                    "clientOptions" => [
+                        'toolbarGroups' => [
+                            ['name' => 'clipboard', 'groups' => ['clipboard', 'undo']],
+                            ['name' => 'editing', 'groups' => ['find', 'selection', 'spellchecker']],
+                            '/',
+                            ['name' => 'basicstyles', 'groups' => ['basicstyles', 'cleanup']],
+                            ['name' => 'colors'],
+                            ['name' => 'paragraph', 'groups' => ['list', 'indent', 'blocks', 'align', 'bidi']],
+                            ['name' => 'links'],
+                            ['name' => 'styles'],
+                            ['name' => 'colors'],
+                            ['name' => 'tools'],
+                            ['name' => 'others'],
+                            
+                        ],
+                        'removeButtons' => 'Subscript,Superscript,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe',
+                        'removePlugins' => 'elementspath',
+                        'resize_enabled' => false
+                    ],
+                ])->label(false) ?>
     <?php endif; ?>
     <?php if($pregunta->tipo == 3): ?>
         <?= $form->field($model, 'file')->fileInput()->label(false) ?>
