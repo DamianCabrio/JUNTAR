9738276401255f9f2443d8f0fe32a4bc8eb727b3
marcosbenitez80 <marcos_benitez80@hotmail.com>
2020-06-30 16:29:54 -0300

    se agrega el botòn para redactar y enviar emaila los participantes.
    se corrigue el msj que acompaña el botòn.

--- frontend/views/evento/verEvento.php
+++ frontend/views/evento/verEvento.php
@@ -143,7 +143,7 @@ $organizadorEmailEvento = $evento->idUsuario0->email;
                                     <i class="material-icons align-middle">today</i><span class=" align-middle"> <?= date("d-m-Y", strtotime($evento->fechaInicioEvento)) ?></span>
                                     <br>
                                      <?php if ($esDueño || $esAdministrador) { ?>
-                                        <i class="material-icons align-middle">email</i><span class=" align-middle"> Escribir un mail</span>
+                                        <i class="material-icons align-middle">email</i><span class=" align-middle"> Escribir un mail a los participantes</span>
 
                                     <?php } ?>
                                     <br><br>
