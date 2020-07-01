a7dd4423c1f40a2f5583ab5cd1526564671a493f
marcosbenitez80 <marcos_benitez80@hotmail.com>
2020-07-01 17:33:48 -0300

    Correciones de enviar mail
    scorreciòn de errores al enviar email.

--- common/mail/enviarEmail-html.php
+++ common/mail/enviarEmail-html.php
@@ -13,19 +13,18 @@ use yii\helpers\Html;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
+
 <body>
 
+     <div class="verify-email" style="text-align: center; padding-top: 30px; padding-bottom: 30px; padding-right: 5px; padding-left:5px;">
 
-    <div class="verify-email" style="text-align: center; padding-top: 30px; padding-bottom: 30px; padding-right: 5px; padding-left:5px;">
- 
-            <p>  <b><?= $mensaje?></b></p>
+        <p>  <b><?= $mensaje?></b></p>
 
-           <div style="margin-top: 15px;">
-                <small><i>(Correo Generado Automáticamente)<i></small>
-           </div>
+        <div style="margin-top: 15px;">
+            <small><i>(Correo Generado Automáticamente)<i></small>
+        </div>
     </div>
-
-    
+  
 </body>
 </html>
 
