b86658b47c3c5b2f86ecf3e9bbef19fda27e9bf5
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-07-01 05:21:59 -0300

    backend update
    Eliminadas funciones js en desuso. Creada nueva vista crearUsuario

---
+++ backend/views/usuario/crearUsuario.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Usuario */
+
+$this->title = 'Crear Nuevo Usuario';
+$this->params['breadcrumbs'][] = ['label' => 'Usuarios', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="usuario-create">
+    <div class="col-12 mb-4 p-0">
+        <div class="card">
+            <h1 class="card-header text-center darkish_bg text-white"><?= Html::encode($this->title) ?></h1>
+
+            <div class="card-body">
+                <?=
+                $this->render('_form', [
+                    'model' => $model,
+                ])
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
