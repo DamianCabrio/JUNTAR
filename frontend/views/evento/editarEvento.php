2bbfcaa049785096152fbc4231dc2bad24242624
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-07-03 18:09:14 -0300

    Corrección de ortografia

--- frontend/views/evento/editarEvento.php
+++ frontend/views/evento/editarEvento.php
@@ -121,7 +121,7 @@ $this->title = "Editar Evento - " . $model->nombreCortoEvento;
 
                     <!-- calendar -->
                     <div id="fechaLimite">
-                        <?= $form->field($model, 'fechaLimiteInscripcion')->input('date', ['style' => 'width:auto'])->label('Fecha límite de pre-inscripción *') ?>
+                        <?= $form->field($model, 'fechaLimiteInscripcion')->input('date', ['style' => 'width:auto'])->label('Fecha límite de preinscripción *') ?>
                     </div>
                     <?= $form->field($model, 'codigoAcreditacion')->textInput(['placeholder' => 'Ingrese código de acreditación'], ['maxlength' => true]) ?>
 
