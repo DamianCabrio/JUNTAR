e68a03953fc76e55b9496891f72c7bdf71ee26a3
n-stange <64751066+n-stange@users.noreply.github.com>
2020-07-04 23:44:09 -0300

    Revamp del about
    Nuevo diseño del about us

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -7,34 +7,252 @@ use yii\helpers\Html;
 $this->title = 'Sobre Nosotros';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about container">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <div class="section_padding">
-        <p><b>Equipo de desarrollo</b></p>
-        <ul>
-            <li>Damian Cabrio</li>
-            <li>Felipe Bastidas</li>
-            <li>Leandro Casanova</li>
-            <li>Norbert Stange</li>
-            <li>Emanuel Araya</li>
-            <li>Laura Murillo</li>
-            <li>Marcos Benitez</li>
-            <li>Mauro Saracini</li>
-            <li>Maximiliano Bajamon</li>
-            <li>Kevin Espinoza</li>
-        </ul>
-        <p><b>Programación Web Avanzada - 2020</b></p>
-        <p>Equipo de Catedra</p>
-        <ul>
-            <li>Natalia Baeza</li>
-            <li>Valeria Zoratto</li>
-        </ul>
-        <p><b><a class="link" href="https://github.com/DamianCabrio/JUNTAR" target="_blank">Link a repositorio
-                    Juntar</a></b></p>
-    </div>
+<section class="" style="min-height: 100vh;">
+	<div class="site-about container">
+		<div class="">
 
-</div>
+
+			<p>Equipo Programación Web Avanzada - 2020</p>
+			<p>Universidad Nacional del Comahue</p>
+			<hr>
+		</div>
+		<div class="">
+			<div class="" style="padding-top:5vh;padding-bottom:5vh">
+				<div class="row">
+					<div class="randomcards col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header super_bg">Damián Cabrío</div>
+
+
+							<div class="card-body darkish_bg text-light">
+								<p>Forrrrrrmulario dinámico. Cuchara en verEvento.php</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="https://github.com/DamianCabrio" target="_blank" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+
+						</div>
+					</div>
+
+					<div class="randomcards col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header super_bg">Felipe Bastidas</div>
+
+							<div class="card-body darkish_bg text-light">
+								<p>"pero-que-a-pasao.mp3" fixes nocturnos.</p>
+								<figure style="transform:scale(0.7)">
+									<figcaption></figcaption>
+									<audio controls src="https://cdn.discordapp.com/attachments/718540792121262151/728816662060728370/pero-que-a-pasao.mp3">
+										Your browser does not support the
+										<code>audio</code> element.
+									</audio>
+								</figure>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="randomcards col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header super_bg">Leandro Casanova</div>
+
+
+							<div class="card-body darkish_bg text-light">
+								<p>Señor ssh master. Permisos usuarios, log in y más.</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="randomcards  col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header super_bg">Norbert Stange</div>
+
+
+							<div class="card-body darkish_bg text-light">
+								<p>Metimos cuchara en verEvento.php</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="randomcards  col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header super_bg">Emanuel Araya</div>
+
+
+							<div class="card-body darkish_bg text-light">
+								<p>Metimos cuchara en verEvento.php</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="randomcards  col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header super_bg">Laura Murillo</div>
+
+
+							<div class="card-body darkish_bg text-light">
+								<p>Metimos cuchara en verEvento.php</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="randomcards  col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header super_bg">Marcos Benitez</div>
+
+							<div class="card-body darkish_bg text-light">
+								<p>Metimos cuchara en verEvento.php</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="randomcards col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header super_bg">Mauro Saracini</div>
+
+							<div class="card-body darkish_bg text-light">
+								<p>Metimos cuchara en verEvento.php</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="randomcards  col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header super_bg">Maximiliano Bajamón</div>
+
+
+							<div class="card-body darkish_bg text-light">
+								<p>Metimos cuchara en verEvento.php</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+					<div class="randomcards  col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header super_bg">Kevin Espinoza</div>
+
+
+							<div class="card-body darkish_bg text-light">
+								<p>Metimos cuchara en verEvento.php</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+					<div class="randomcardsProfes col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header dark_bg"><span class="text-light">Natalia Baeza</span></div>
+
+							<div class="card-body darkish_bg text-light">
+								<p>Profesora Cátedra PWA - 2020</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+					<div class="randomcardsProfes col-sm-12 col-md-3 mb-4">
+						<div class="card shadow">
+							<div class="card-header dark_bg"><span class="text-light">Valeria Zoratto</span></div>
+
+
+							<div class="card-body darkish_bg text-light">
+								<p>Profesora Cátedra PWA - 2020</p>
+
+							</div>
+							<div class="card-footer darkish_bg text-center">
+								<a href="" class="text-light link"><i class="material-icons">contact_mail</i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<div>
+			<hr>
+			<p><b><a class="link" href="https://github.com/DamianCabrio/JUNTAR" target="_blank">Link a repositorio Juntar</a></b></p>
+			<hr>
+		</div>
+		<!-- Button trigger modal -->
+		<a class="float-right" data-toggle="modal" data-target="#exampleModalLong">
+			<small>la verdá</small>
+		</a>
+
+		<!-- Modal -->
+		<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-body">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/upL2HCdkBSc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--
+			<h1> /* Html::encode($this->title) ?></h1>
+		<div class="section_padding">
+			<p><b>Equipo de desarrollo</b> </p>
+			<ul>
+				<li>Damian Cabrio</li>
+				<li>Felipe Bastidas</li>
+				<li>Leandro Casanova</li>
+				<li>Norbert Stange</li>
+				<li>Emanuel Araya</li>
+				<li>Laura Murillo</li>
+				<li>Marcos Benitez</li>
+				<li>Mauro Saracini</li>
+				<li>Maximiliano Bajamon</li>
+				<li>Kevin Espinoza</li>
+			</ul>
+			<p><b>Programación Web Avanzada - 2020</b></p>
+			<p>Equipo de Catedra</p>
+			<ul>
+				<li>Natalia Baeza</li>
+				<li>Valeria Zoratto</li>
+			</ul>
+			
+		</div>
+-->
+
+
+	</div>
+</section>
+
 <!-- 用大量的工作和爱创造的页面 !-->
 <!-- 素晴らしいチーム、これは10/10です !-->
 <!-- Von hier zu den Sternen ★★★★★ -->
-
