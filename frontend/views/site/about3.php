66451b3f86b7fe819643ba9019010807fb9ece3b
HiDrAWaRx <fbastidas_94@hotmail.com>
2020-07-13 09:28:26 -0300

    no sé que es
    pero lo subo por si acaso xd

---
+++ frontend/views/site/about3.php
@@ -0,0 +1,248 @@
+<?php
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Sobre Nosotros';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<section class="" style="min-height: 100vh;">
+    <div class="site-about container">
+        <div class="">
+            <p>Equipo Programación Web Avanzada - 2020</p>
+            <hr>
+        </div>
+        <div class="">
+            <div class="row">
+                <div class="randomcards col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header super_bg">Damián Cabrío</div>
+
+
+                        <div class="card-body darkish_bg text-light">
+                            <p id="descripcionDC"></p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="dc" target="_blank" class="text-light linkAbout" data-id="Damián Cabrio"><i class="material-icons">contact_mail</i></a>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="randomcards col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header super_bg">Felipe Bastidas</div>
+
+                        <div class="card-body darkish_bg text-light">
+                            <p id="descripcionFB"></p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="fb" class="text-light linkAbout" data-id="Felipe Bastidas"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="randomcards col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header super_bg">Leandro Casanova</div>
+
+
+                        <div class="card-body darkish_bg text-light">
+                            <p>Señor ssh master. Permisos usuarios, log in y más.</p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="lc" class="text-light linkAbout " data-id="Leandro Casanova"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="randomcards  col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header super_bg">Norbert Stange</div>
+
+
+                        <div class="card-body darkish_bg text-light">
+                            <p id="descriptionNS"></p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="ns" class="text-light linkAbout getlinks" data-id="Norbert Strange"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="randomcards  col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header super_bg">Emanuel Araya</div>
+
+
+                        <div class="card-body darkish_bg text-light">
+                            <p>Metimos cuchara en verEvento.php</p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="ea" class="text-light linkAbout" data-id="Emanuel Araya"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="randomcards  col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header super_bg">Laura Murillo</div>
+
+                        <div class="card-body darkish_bg text-light">
+                            <p id="descriptionLM"></p>
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="lm" class="text-light linkAbout" data-id="Laura Murillo"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="randomcards  col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header super_bg">Marcos Benitez</div>
+
+                        <div class="card-body darkish_bg text-light">
+                            <p>Metimos cuchara en verEvento.php</p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="mbe" class="text-light linkAbout" data-id="Marcos Benitez"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="randomcards col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header super_bg">Mauro Saracini</div>
+
+                        <div class="card-body darkish_bg text-light">
+                            <p>Metimos cuchara en verEvento.php</p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="ms" class="text-light linkAbout" data-id="Mauro Saracini"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="randomcards  col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header super_bg">Maximiliano Bajamón</div>
+
+
+                        <div class="card-body darkish_bg text-light">
+                            <p>A la grande le puse Cuca</p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="mba" class="text-light linkAbout" data-id="Maximiliano Bajamon"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="randomcards  col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header super_bg">Kevin Espinoza</div>
+
+
+                        <div class="card-body darkish_bg text-light">
+                            <!-- Izi pizi-->
+                            <p id="KevinMekuruTheBassistAndGamer-ahre"></p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="me" class="text-light linkAbout" data-id="Kevin Espinoza"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="randomcardsProfes col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header dark_bg"><span class="text-light">Natalia Baeza</span></div>
+
+                        <div class="card-body darkish_bg text-light">
+                            <p>Profesora Cátedra PWA - 2020</p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="nb" class="text-light linkAbout" data-id="Natalia Baeza"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="randomcardsProfes col-sm-12 col-md-3 mb-4">
+                    <div class="card shadow">
+                        <div class="card-header dark_bg"><span class="text-light">Valeria Zoratto</span></div>
+
+
+                        <div class="card-body darkish_bg text-light">
+                            <p>Profesora Cátedra PWA - 2020</p>
+
+                        </div>
+                        <div class="card-footer darkish_bg text-center">
+                            <a id="vz" class="text-light linkAbout" data-id="Valeria Zoratto"><i class="material-icons">contact_mail</i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div>
+            <hr>
+            <p><b><a class="link" href="https://github.com/DamianCabrio/JUNTAR" target="_blank">Link a repositorio Juntar</a></b> Licencia GNU GPL version 3</p>
+            <hr>
+        </div>
+
+        <a class="float-right link" href="https://www.youtube.com/embed/upL2HCdkBSc" target="_blank">
+            <small>.</small>
+        </a>
+
+
+        <!-- Modal -->
+        <div class="modal fade p-0" id="aboutUsModal" tabindex="-1" role="dialog" aria-labelledby="labelAboutUsModal" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal" role="text">
+                <div class="modal-content super_bg">
+                    <h2 class="modal-title p-2 text-white text-center" id="aboutUsTitle"></h2>
+                    <hr class="text-white">
+                    <div id="modal_content" class="modal-body">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--
+                <h1> /* Html::encode($this->title) ?></h1>
+        <div class="section_padding">
+                <p><b>Equipo de desarrollo</b> </p>
+                <ul>
+                        <li>Damian Cabrio</li>
+                        <li>Felipe Bastidas</li>
+                        <li>Leandro Casanova</li>
+                        <li>Norbert Stange</li>
+                        <li>Emanuel Araya</li>
+                        <li>Laura Murillo</li>
+                        <li>Marcos Benitez</li>
+                        <li>Mauro Saracini</li>
+                        <li>Maximiliano Bajamon</li>
+                        <li>Kevin Espinoza</li>
+                </ul>
+                <p><b>Programación Web Avanzada - 2020</b></p>
+                <p>Equipo de Catedra</p>
+                <ul>
+                        <li>Natalia Baeza</li>
+                        <li>Valeria Zoratto</li>
+                </ul>
+                
+        </div>
+        -->
+
+
+    </div>
+</section>
+
+<!-- 用大量的工作和爱创造的页面 !-->
+<!-- 素晴らしいチーム、これは10/10です !-->
+<!-- Von hier zu den Sternen ★★★★★ -->
